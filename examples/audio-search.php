bbb5c8a4f82bd78caded29efe82b23360328ac88
Keith Casey <keith@caseysoftware.com>
2014-04-07 14:37:11 -0500

    wired the search results to work more simply

--- examples/audio-search.php
+++ examples/audio-search.php
@@ -5,11 +5,18 @@ require '../vendor/autoload.php';
 
 $audio = new Op3nvoice\Audio($apikey);
 
-$items = $audio->search('does');
+$result = $audio->search('close');
 
-foreach($items as $item) {
+$results = $result['item_results'];
+$items = $result['_links']['item'];
+foreach($items as $index => $item) {
     $bundle = $audio->load($item['href']);
 
     echo $bundle['_links']['self']['href'] . "\n";
     echo $bundle['name'] . "\n";
+
+    $search_hits = $results[$index]['term_results'][0]['matches'][0]['hits'];
+    foreach($search_hits as $search_hit) {
+        echo $search_hit['start'] . ' -- ' . $search_hit['end'] . "\n";
+    }
 }
