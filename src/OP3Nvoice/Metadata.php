63427fd23992ebfc083adc19a7370ee06d7a2a0d
Keith Casey <keith@caseysoftware.com>
2014-04-23 21:59:36 -0500

    wired deleting the metadata

--- src/OP3Nvoice/Metadata.php
+++ src/OP3Nvoice/Metadata.php
@@ -45,4 +45,21 @@ class Metadata extends Client
 
         return $response->isSuccessful();
     }
+
+    public function delete($id)
+    {
+        $request = $this->client->get($id, array(), array('exceptions' => false));
+        $request->addHeader('Authorization', $this->apiKey);
+        $response = $request->send();
+        $bundle = $response->json();
+        $metadataURI = $bundle['_links']['o3v:metadata']['href'];
+
+        $request = $this->client->delete($metadataURI, array(), '', array('exceptions' => false));
+
+        $request->addHeader('Authorization', $this->apiKey);
+        $response = $request->send();
+        $this->detail = $response->json();
+
+        return $response->isSuccessful();
+    }
 }
