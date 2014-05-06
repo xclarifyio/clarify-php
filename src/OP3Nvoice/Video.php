16e9292d335a2c738b86071d2d83748240246420
Keith Casey <keith@caseysoftware.com>
2014-05-06 16:04:27 -0400

    placeholders for the video bundles

---
+++ src/OP3Nvoice/Video.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace OP3Nvoice;
+
+use OP3Nvoice\Metadata;
+use OP3Nvoice\Exceptions\InvalidJSONException;
+use OP3Nvoice\Exceptions\InvalidResourceException;
+
+class Video extends Client
+{
+    public $detail = null;
+
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function load($id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+
+    }
+
+    public function search($query, $limit = 10, $embed = '', $iterator = '')
+    {
+
+    }
+}
