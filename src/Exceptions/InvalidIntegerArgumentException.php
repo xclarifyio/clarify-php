aff5a9fce4f64a00995a163468f655d1e07d8cb0
Luis Cordova <cordoval@gmail.com>
2014-06-07 18:05:43 -0500

    fix conflicts and revert js cs fixing

--- src/Exceptions/InvalidIntegerArgumentException.php
+++ src/Exceptions/InvalidIntegerArgumentException.php
@@ -2,11 +2,7 @@
 
 namespace OP3Nvoice\Exceptions;
 
-<<<<<<< HEAD
-class InvalidIntegerArgumentException extends \Exception {}
-=======
 class InvalidIntegerArgumentException extends \Exception
 {
 
 }
->>>>>>> 1f09493... php-cs-fixer fix .
--- src/Exceptions/InvalidResourceException.php
+++ src/Exceptions/InvalidResourceException.php
@@ -2,11 +2,7 @@
 
 namespace OP3Nvoice\Exceptions;
 
-<<<<<<< HEAD
-class InvalidResourceException extends \Exception {}
-=======
 class InvalidResourceException extends \Exception
 {
 
 }
->>>>>>> 1f09493... php-cs-fixer fix .
