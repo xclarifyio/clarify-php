f142632eb259e7b631206f86ecbef38277916b38
Luis Cordova <cordoval@gmail.com>
2014-06-07 18:46:40 -0500

    plug first test

---
+++ tests/Services/GuzzleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OP3Nvoice\Tests\Services;
+
+class GuzzleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testX()
+    {
+        $this->assertTrue(true);
+    }
+}
