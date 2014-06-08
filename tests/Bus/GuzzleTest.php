735c3e6341bb645fb2a27f6c857936e5353bda31
Luis Cordova <cordoval@gmail.com>
2014-06-08 13:43:17 -0600

    plugging myself

--- tests/Bus/GuzzleTest.php
+++ tests/Bus/GuzzleTest.php
@@ -2,8 +2,6 @@
 
 namespace OP3Nvoice\Tests\Services;
 
-use OP3Nvoice\Bus\AuthenticatorSubscriber;
-
 class GuzzleTest extends \PHPUnit_Framework_TestCase
 {
     public function it_is_authenticated()
