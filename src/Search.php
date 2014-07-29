e157cd5b0c79329a6c3fa98d2ea17f9a60d4c122
Keith Casey <keith@caseysoftware.com>
2014-07-29 14:40:40 -0500

    finished updating the namespace information

--- src/Search.php
+++ src/Search.php
@@ -2,32 +2,4 @@
 
 namespace OP3Nvoice;
 
-class Search extends Client
-{
-    public $detail = null;
-
-    /**
-     * @param $query
-     * @param int $limit
-     * @param string $embed
-     * @param string $iterator
-     *
-     * @return array|bool|float|int|string
-     */
-    public function search($query, $limit = 10, $params = array())
-    {
-        $request = $this->client->get('/v1/search', array(), array('exceptions' => false));
-
-        $request->getQuery()->set('query', $query);
-        $request->getQuery()->set('limit', $limit);
-        foreach($params as $key => $value) {
-            $request->getQuery()->set($key, $value);
-        }
-
-        $response = $this->process($request);
-
-        $this->detail = $response->json();
-
-        return $response->json();
-    }
-}
+class Search extends \Clarify\Search { }
--- src/Subresource.php
+++ src/Subresource.php
@@ -2,41 +2,4 @@
 
 namespace OP3Nvoice;
 
-abstract class Subresource extends Client
-{
-    /**
-     * @param $id
-     * @return mixed
-     */
-    protected function getSubresourceURI($id)
-    {
-        $request = $this->client->get($id, array(), array('exceptions' => false));
-        $response = $this->process($request);
-
-        $bundle = $response->json();
-
-        return $bundle['_links'][$this->subresource]['href'];
-    }
-
-    /**
-     * @param $id
-     * @return array|bool|float|int|string
-     */
-    public function load($id)
-    {
-        $resourceURI = $this->getSubresourceURI($id);
-
-        return parent::load($resourceURI);
-    }
-
-    /**
-     * @param $id
-     * @return bool
-     */
-    public function delete($id)
-    {
-        $resourceURI = $this->getSubresourceURI($id);
-
-        return parent::delete($resourceURI);
-    }
-}
+abstract class Subresource extends \Clarify\Subresource { }
