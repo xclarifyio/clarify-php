ba5350edbf9f189bcfa65a19a0326c6c1c223eba
Keith Casey <keith@caseysoftware.com>
2014-07-29 14:08:50 -0500

    updated the namespace of the library

---
+++ src/Clarify/Bundle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Clarify;
+
+class Bundle extends Client
+{
+    public function create($name, $media_url = '', $metadata = '', $notify_url = '', $audio_channel = '')
+    {
+        $params = array();
+        $params['name'] = $name;
+        $params['media_url'] = $media_url;
+        $params['metadata'] = $metadata;
+        $params['notify_url'] = $notify_url;
+        $params['audio_channel'] = $audio_channel;
+
+        return $this->post($params);
+    }
+
+    public function update($id, $name = '', $notify_url = '', $version  = 1)
+    {
+        $params = array();
+        $params['id'] = $id;
+        $params['name'] = $name;
+        $params['notify_url'] = $notify_url;
+        $params['version'] = $version;
+
+        return $this->put($params);
+    }
+}
---
+++ src/Clarify/Exceptions/InvalidResourceException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Clarify\Exceptions;
+
+class InvalidResourceException extends \Exception
+{
+
+}
