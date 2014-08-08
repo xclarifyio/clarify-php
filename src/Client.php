8d216c68d62e93dbad97a8031caa26d7f253eb0f
Keith Casey <keith@caseysoftware.com>
2014-08-08 16:10:46 -0500

    eliminating some old unneeded code

--- src/Client.php
+++ src/Client.php
@@ -10,8 +10,4 @@ use OP3Nvoice\Tracks;
 use OP3Nvoice\Exceptions\InvalidJSONException;
 use OP3Nvoice\Exceptions\InvalidResourceException;
 
-/**
- * This is the base class that all of the individual media-related classes extend. At the moment, it simply initializes
- *   the connection by setting the user agent and the base URI for the API calls.
- */
 abstract class Client extends \Clarify\Client { }
--- src/Metadata.php
+++ src/Metadata.php
@@ -2,6 +2,4 @@
 
 namespace OP3Nvoice;
 
-use OP3Nvoice\Exceptions\InvalidJSONException;
-
 class Metadata extends \Clarify\Subresource { }
