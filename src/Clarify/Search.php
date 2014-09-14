4bd8a1d46cf27ff21cdcc52eca4167657d5916a7
Keith Casey <keith@caseysoftware.com>
2014-09-13 22:41:58 -0500

    wiring in the iterator handling

--- src/Clarify/Search.php
+++ src/Clarify/Search.php
@@ -14,14 +14,16 @@ class Search extends Client
      * @param $query
      * @param int $limit
      * @param array $params
+     * @param string $iterator
      * @return array|bool|float|int|string
      */
-    public function search($query, $limit = 10, $params = array())
+    public function search($query, $limit = 10, $params = array(), $iterator = '')
     {
         $request = $this->client->get('/v1/search', array(), array('exceptions' => false));
 
         $request->getQuery()->set('query', $query);
         $request->getQuery()->set('limit', $limit);
+        $request->getQuery()->set('iterator', $iterator);
         foreach($params as $key => $value) {
             $request->getQuery()->set($key, $value);
         }
