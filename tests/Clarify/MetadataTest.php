eabaf2cc7c442c1cfcc6e5e07ed7121ae0eae5c6
Keith Casey <keith@caseysoftware.com>
2014-09-18 23:48:45 -0500

    stubbing out the tests for implementation

---
+++ tests/Clarify/MetadataTest.php
@@ -0,0 +1,39 @@
+<?php
+include '../vendor/autoload.php';
+
+use Guzzle\Http;
+
+class MetadataTest extends PHPUnit_Framework_TestCase
+{
+    protected $bundle = null;
+    protected $media  = 'http://media.clarify.io/audio/samples/harvard-sentences-1.wav';
+
+    public function setUp()
+    {
+        global $apikey;
+
+        $this->bundle = new \Clarify\Bundle($apikey);
+
+        parent::setUp();
+    }
+
+    public function testUpdate()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetSubresourceURI()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testLoad()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testDelete()
+    {
+        $this->markTestIncomplete();
+    }
+}
