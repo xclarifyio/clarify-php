592b88ce0d5c9915af9918b2a12ef2ca8cbb581b
Keith Casey <keith@caseysoftware.com>
2014-11-09 00:27:17 -0600

    wired the metadata update test using the bundle

--- tests/Clarify/MetadataTest.php
+++ tests/Clarify/MetadataTest.php
@@ -13,6 +13,7 @@ class MetadataTest extends PHPUnit_Framework_TestCase
         global $apikey;
 
         $this->client = new \Clarify\Client($apikey);
+        $this->bundle = new \Clarify\Bundle($apikey, $this->client);
         $this->metadata = new \Clarify\Metadata($this->client);
 
         parent::setUp();
@@ -34,5 +35,20 @@ class MetadataTest extends PHPUnit_Framework_TestCase
     {
         $params = array('version' => 'not an integer');
         $this->metadata->update($params);
+    }
+
+    public function testUpdate()
+    {
+        $name = 'name - testMetadataUpdate';
+        $result = $this->bundle->create($name, $this->media);
+        $this->assertEquals(201, $this->bundle->getStatusCode());
+        $location = $result->getHeader('Location');
+
+        $params = array('data' => '{"name" : "value"}', 'id' => $location);
+        $result = $this->bundle->metadata->update($params);
+        $this->assertTrue($result);
+
+        $this->assertTrue($this->bundle->delete($location));
+        $this->assertEquals(204, $this->bundle->getStatusCode());
     }
 }
