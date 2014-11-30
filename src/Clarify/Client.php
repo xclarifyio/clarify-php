a6eae581fa2591c194bcaad4076cba8381fb241f
Keith Casey <keith@caseysoftware.com>
2014-11-30 14:09:14 -0600

    simplifying the return types

--- src/Clarify/Client.php
+++ src/Clarify/Client.php
@@ -3,8 +3,6 @@
 namespace Clarify;
 
 use Guzzle\Http;
-use Clarify\Exceptions\InvalidJSONException;
-use Clarify\Exceptions\InvalidEnumTypeException;
 use Clarify\Exceptions\InvalidIntegerArgumentException;
 
 
@@ -20,7 +18,7 @@ use Clarify\Exceptions\InvalidIntegerArgumentException;
  */
 class Client
 {
-    const USER_AGENT = 'clarify-php/1.1.0';
+    const USER_AGENT = 'clarify-php/1.2.0';
 
     protected $baseURI  = 'https://api.clarify.io/v1/';
     protected $apiKey   = '';
@@ -58,49 +56,6 @@ class Client
         $request->setHeader('Authorization', 'Bearer ' . $this->apiKey);
         $this->response =  $request->send();
         $this->statusCode = $this->response->getStatusCode();
-
-        return $this->response;
-    }
-
-    /**
-     * @param $url
-     * @param array $options
-     * @return Http\Message\Response|null
-     */
-    public function post($url, array $options = array())
-    {
-        /** @var $request \Guzzle\Http\Message\Request */
-        $request = $this->client->post($url, array(), '', array('exceptions' => false));
-        foreach($options as $key => $value) {
-            $request->setPostField($key, $value);
-        }
-
-        $response = $this->process($request);
-        $this->detail = $response->json();
-
-        return $response;
-    }
-
-    /**
-     * @param $url
-     * @param array $options
-     * @throws Exceptions\InvalidIntegerArgumentException
-     * @return mixed
-     */
-    public function put($url, array $options)
-    {
-        $version = isset($options['version']) ? $options['version'] : '1';
-        if (!is_numeric($version)) {
-            throw new InvalidIntegerArgumentException();
-        }
-
-        $request = $this->client->put($url, array(), '', array('exceptions' => false));
-        unset($options['id']);
-        foreach($options as $key => $value) {
-            $request->setPostField($key, $value);
-        }
-
-        $this->response = $this->process($request);
         $this->detail = $this->response->json();
 
         return $this->response->isSuccessful();
@@ -108,12 +63,50 @@ class Client
 
     /**
      * @param $url
+     * @param array $options
+     * @return Http\Message\Response|null
+     */
+    public function post($uri, array $options = array())
+    {
+        /** @var $request \Guzzle\Http\Message\Request */
+        $request = $this->client->post($uri, array(), '', array('exceptions' => false));
+        foreach($options as $key => $value) {
+            $request->setPostField($key, $value);
+        }
+
+        return $this->process($request);
+    }
+
+    /**
+     * @param $url
+     * @param array $options
+     * @return bool
+     * @throws Exceptions\InvalidIntegerArgumentException
+     */
+    public function put($uri, array $options)
+    {
+        $version = isset($options['version']) ? $options['version'] : '1';
+        if (!is_numeric($version)) {
+            throw new InvalidIntegerArgumentException();
+        }
+
+        $request = $this->client->put($uri, array(), '', array('exceptions' => false));
+        unset($options['id']);
+        foreach($options as $key => $value) {
+            $request->setPostField($key, $value);
+        }
+
+        return $this->process($request);
+    }
+
+    /**
+     * @param $url
      * @param array $parameters
      * @return array|bool|float|int|string
      */
-    public function get($url, array $parameters = array())
+    public function get($uri, array $parameters = array())
     {
-        $request = $this->client->get($url, array(), array('exceptions' => false));
+        $request = $this->client->get($uri, array(), array('exceptions' => false));
         foreach($parameters as $key => $value) {
             $request->getQuery()->set($key, $value);
         }
@@ -129,13 +122,10 @@ class Client
      * @param $id
      * @return bool
      */
-    public function delete($id)
+    public function delete($uri)
     {
-        $request = $this->client->delete($id, array(), '', array('exceptions' => false));
-        $response = $this->process($request);
+        $request = $this->client->delete($uri, array(), '', array('exceptions' => false));
 
-        $this->detail = $response->json();
-
-        return $response->isSuccessful();
+        return $this->process($request);
     }
 }
