da585f2353a5ac6a88af71a7efaced75380603ca
Keith Casey <keith@caseysoftware.com>
2015-01-13 15:49:31 -0600

    making the sample more clear

--- examples/create.php
+++ examples/create.php
@@ -7,7 +7,7 @@ require __DIR__.'/../vendor/autoload.php';
 $bundle = new \Clarify\Bundle($apikey);
 
 $success = $bundle->create(
-    'name' . rand(0, 500),
+    "Dorothy and the Wizard of Oz",
     'http://media.clarify.io/audio/books/dorothyandthewizardinoz_01_baum_64kb.mp3'
 );
 
