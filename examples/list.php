bfa17fb821545e664f5eb9312335c526432bcc72
Keith Casey <keith@caseysoftware.com>
2015-02-26 10:02:35 -0600

    simplified the loop

--- examples/list.php
+++ examples/list.php
@@ -6,13 +6,11 @@ require __DIR__.'/../vendor/autoload.php';
 
 $bundle = new \Clarify\Bundle($apikey);
 
-$page = $bundle->index();
+$firstpage = $bundle->index();
 
 while ($bundle->hasMorePages()) {
-    $items = $page['_links']['items'];
-
-    foreach ($items as $item) {
-        $_bundle = $bundle->load($item['href']);
+    foreach ($bundle as $bundle_id) {
+        $_bundle = $bundle->load($bundle_id);
 
         echo $_bundle['_links']['self']['href'] . "\n";
         echo $_bundle['name'] . "\n";
