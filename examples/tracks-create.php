dd5c9a56436bb1c63bc17a886422c0244f9399fa
Keith Casey <keith@caseysoftware.com>
2016-01-03 22:09:24 -0600

    fixed the poor example

--- examples/tracks-create.php
+++ examples/tracks-create.php
@@ -5,38 +5,35 @@ require __DIR__ . '/credentials.php';
 require __DIR__.'/../vendor/autoload.php';
 
 $bundle = new \Clarify\Bundle($apikey);
-
 $results = $bundle->index();
-$items = $results['_links']['items'];
 
 /**
  * This gets the first item from our list of bundles, shows the existing tracks, adds three audio tracks, and then
  *   shows the new list of tracks.
  */
-foreach ($items as $item) {
-    $tracks = $bundle->tracks->load($item['href']);
-    print_r($tracks);
+$item = $results['_links']['items'][0];
 
-    $success = $bundle->tracks->create(
-        array(
-            'id' => $item['href'],
-            'media_url' => 'http://media.clarify.io/audio/samples/harvard-sentences-1.wav',
-        )
-    );
-    $success = $bundle->tracks->create(
-        array(
-            'id' => $item['href'],
-            'media_url' => 'http://media.clarify.io/audio/samples/harvard-sentences-2.wav',
-        )
-    );
-    $success = $bundle->tracks->create(
-        array(
-            'id' => $item['href'],
-            'media_url' => 'http://media.clarify.io/audio/books/dorothyandthewizardinoz_01_baum_64kb.mp3',
-        )
-    );
+$tracks = $bundle->tracks->load($item['href']);
+print_r($tracks);
 
-    $tracks = $bundle->tracks->load($item['href']);
-    print_r($tracks);
-    die();
-}
+$success = $bundle->tracks->create(
+    array(
+        'id' => $item['href'],
+        'media_url' => 'http://media.clarify.io/audio/samples/harvard-sentences-1.wav',
+    )
+);
+$success = $bundle->tracks->create(
+    array(
+        'id' => $item['href'],
+        'media_url' => 'http://media.clarify.io/audio/samples/harvard-sentences-2.wav',
+    )
+);
+$success = $bundle->tracks->create(
+    array(
+        'id' => $item['href'],
+        'media_url' => 'http://media.clarify.io/audio/books/dorothyandthewizardinoz_01_baum_64kb.mp3',
+    )
+);
+
+$tracks = $bundle->tracks->load($item['href']);
+print_r($tracks);
